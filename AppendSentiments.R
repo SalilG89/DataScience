583088d85c1100ab75162e8575be0d278068cdc8
SalilG89 <salilg89@gmail.com>
2016-03-08 09:36:47 -0500

    Added files via upload

--- AppendSentiments.R
+++ AppendSentiments.R
@@ -1,10 +1,17 @@
 install.packages("syuzhet")
 library(syuzhet)
 
+library(dplyr)
+devtools::install_github("hrbrmstr/streamgraph")
+install.packages("streamgraph")
 
+
+library(streamgraph)
+library(viridis)
+
+##Read in Flat File produced by python scripts
 mypipeff <- read.table("H:/PY/bernietweetsff.txt", sep = "|", header = TRUE)
 head(mypipeff,1)
-##colnames(mydf) <- c("id", "text", 'createdtime')
 
 ## Convert Factor Columns to Character Columns
 i <- sapply(mypipeff, is.factor)
@@ -12,14 +19,45 @@ mypipeff[i] <- lapply(mypipeff[i], as.character)
 
 dfgooddates <- mypipeff
 
+
+
+##Format Twitter Created_at 
 dfgooddates$cleantime <- as.POSIXct(dfgooddates$created_at, format = "%a %b %d %H:%M:%S +0000 %Y")
 
 ##head(dfgooddates,1)
 
 ##Derive Sentiments using Syuzhet package 
-tweentsentiments <- get_nrc_sentiment(dfgooddates$tweet)
+tweetsentiments <- get_nrc_sentiment(dfgooddates$tweet)
 
 DFwSentiments <- data.frame(dfgooddates$id, dfgooddates$cleantime, tweetsentiments)
+
+
+##head(tweetsentiments,1)
+
+write.csv(DFwSentiments, file = "TweetSentiments2.csv")
+
+##Convert sentiment columns to rows
+pivotDF <- melt(DFwSentiments, id.vars = c("dfgooddates.id", "dfgooddates.cleantime"))
+
+## Write 
+write.csv(pivotDF, file = "pivotsentiments.csv")
+
+
+
+
+
+pivotDF %>% 
+  mutate(date=as.Date(dfgooddates.cleantime, format="%m/%d/%y")) %>% 
+  streamgraph(key="variable", value="value", offset="expand") %>% 
+  ##sg_fill_manual(stock_colors) %>% 
+  ##sg_axis_x(tick_interval=10, tick_units="s") %>% 
+  sg_legend(TRUE, "Ticker: ")
+
+
+
+
+
+
 
 #typeof(mypipeff[[1,2]])
 #class(mypipeff[[1,2]])
@@ -37,32 +75,3 @@ DFwSentiments <- data.frame(dfgooddates$id, dfgooddates$cleantime, tweetsentimen
 ##head(DFwSentiments,1)
 ##typeof(DFwSentiments[[1,2]])
 ##class(DFwSentiments[[1,2]])
-
-
-summary(tweetsentiments)
-write.csv(DFwSentiments, file = "TweetSentiments.csv")
-
-##Convert sentiment columns to rows
-newdf <- melt(DFwSentiments, id.vars = c("dfgooddates.id", "dfgooddates.cleantime"))
-
-
-str(newdf)
-
-library(dplyr)
-devtools::install_github("hrbrmstr/streamgraph")
-install.packages("streamgraph")
-
-
-library(streamgraph)
-library(viridis)
-
-head(newdf,5)
-
-
-
-newdf %>% 
-  mutate(date=as.Date(dfgooddates.cleantime, format="%m/%d/%y")) %>% 
-  streamgraph(key="variable", value="value", offset="expand") %>% 
-  ##sg_fill_manual(stock_colors) %>% 
-  ##sg_axis_x(tick_interval=10, tick_units="s") %>% 
-  sg_legend(TRUE, "Ticker: ")
