1f71bc1a4b306a32b7a5c7066c75f3181ecf9f87
SalilG89 <salilg89@gmail.com>
2017-06-05 18:10:10 -0400

    Add files via upload

--- fitbitetl.R
+++ fitbitetl.R
@@ -1,12 +1,7 @@
 rm(list = ls())
 
-differenceofdays <- as.integer(difftime(Sys.Date(), strptime("1.05.2017", format = "%d.%m.%Y"),   units="days"))
-
-
-
-
-
-
+differenceofdays <- as.integer(difftime(Sys.Date(), 
+                    strptime("1.05.2017", format = "%d.%m.%Y"),units="days"))
 
 
 start <- as.POSIXct("2017-05-01")
@@ -24,27 +19,13 @@ datesdf<- as.data.frame(posdates)
 datesdf$formatdates <- as.character(as.Date(datesdf$posdates, "%Y%M/%D"))
 
 
-
-datesdf$formatdates[1]
-
-
-
-
-
-
-
 library(fitbitScraper)
 library(ggplot2)
-library(stringr, lib.loc = "C:\\rw\\packages\\library\\")
 library(dplyr)
-install.packages("tibble")
-install.packages("purrr")
 
-install.packages("rlang")
-library(rlang)
 library(tibble)
-library(lazyeval)
-install.packages("lazyeval")
+
+
 
 rundate <- datesdf$formatdates[1]
 
@@ -53,10 +34,9 @@ rundate <- datesdf$formatdates[1]
 dfsteps <- get_intraday_data(fitbitcookies, what="steps", date= rundate)
 dfheartrate <- get_intraday_data(fitbitcookies, what="heart-rate", date= rundate)
 
-library(purrr)
+
 library(tibble)
 
-map2
 
 get_HeartData <- function(chardate) {
   print(chardate)
@@ -86,14 +66,3 @@ datestibble <- datestibble %>%
   mutate(heartdata = map(formatdates, get_HeartData))%>% 
   mutate(stepsdata = map(formatdates, get_stepsdata))
 datestibble
-install.packages("tidyr")
-library(tidyr)
-datestibble %>% unest()
-
-library(tidyr)
-library(tibble)
-
-datestibble$heartdata[1]
-install.packages("tibble")
-
-
